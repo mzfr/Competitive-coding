bc29c694ed4ff45aabf21933291af3e6eaa8f754
mzfr <Mehtab.zafar98@gmail.com>
2020-10-23 16:03:17 +0530

    Add merge sort

---
+++ sorting/mergeSort/mergeSort.py
@@ -0,0 +1,33 @@
+def merge(left: list, right: list):
+    i = j = 0
+    result = []
+
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+
+    result += left[i:]
+    result += right[j:]
+    return result
+
+
+def merge_sort(arr: list):
+
+    if len(arr) < 1:
+        return arr
+
+    mid = len(arr) // 2
+
+    left = merge_sort(arr[:mid])
+    right = merge_sort(arr[mid:])
+
+    return merge(left, right)
+
+
+if __name__ == "__main__":
+    arr = []
+    merge_sort(arr)
---
+++ sorting/mergeSort/readme.md
@@ -0,0 +1,6 @@
+## Merge Sort
+
+* Merge Sort recursively splits the array in half till each element is a single value-d array.
+    - Since a single value is always sorted.
+
+* Then it merges all the elements comparing which element to place first in the resultant array.
